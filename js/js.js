3fc17e3a6ef6f6f5355ba4bb9332806bc0be2a48
Lucas Camino <lucascamino05@gmail.com>
2021-09-01 20:38:06 -0300

    funcionalidad botones bootstrap, href internos

--- js/js.js
+++
@@ -1,9 +0,0 @@
-const open=document.getElementById('open');
-const model_container=document.getElementById('modal_container');
-const close=document.getElementById('close');
-
-open.addEventListener('click',()=>{
-  modal_container.classList.add('show');
-});
-close.addEventListener('click',()=>{model_container.classList.remove('show');
-});
